82431840216681c3750cf836f7292f60494fcc61
hxl520 <107551700124@stu.xju.edu.cn>
2019-07-05 20:28:38 +0800

    Add files via upload

--- ExamplePackage/src/ExamplePackage.jl
+++ ExamplePackage/src/ExamplePackage.jl
@@ -2,4 +2,14 @@ module ExamplePackage
 
 greet() = print("Hello World!")
 
-end # module
+using Expectations, Distributions
+
+function foo(μ = 1., σ = 2.)
+    d = Normal(μ, σ)
+    E = expectation(d)
+    return E(x -> sin(x))
+end
+
+export foo
+
+end 
